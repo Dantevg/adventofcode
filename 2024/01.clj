25e828da9d1fa2fb64b166ac4716bed0c2c12310
RedPolygon <mail@dantevg.nl>
2024-12-02 14:50:50 +0100

    Small cleanup

--- 2024/01.clj
+++ 2024/01.clj
@@ -1,9 +1,6 @@
 (ns adventofcode.2024.01
   (:require [clojure.string :as str]))
 
-;; does clojure seriously not have an identity function?
-(defn id [& x] x)
-
 ;; also works as unzip (lisp magic? maybe this should be called transpose)
 (defn zip [x]
   (apply map list x))
@@ -14,26 +11,22 @@
 (defn parse-file [file]
   (map parse-line (str/split-lines file)))
 
-(defn pair-numbers [pairs]
-  (zip (map sort (zip pairs))))
-
-(defn abs-diff [pair]
-  (abs (- (first pair) (second pair))))
-
-(defn n-occurrences [of in]
-  (count (filter #{of} in)))
-
-(defn similarity-score [of in]
-  (* of (n-occurrences of in)))
+(defn similarity-score [n in]
+  (->> in (filter #{n}) (count) (* n)))
 
 (defn f1 []
   (let [in (parse-file (slurp *in*))
-        pairs (pair-numbers in)]
-    (apply + (map abs-diff pairs))))
+        pairs (zip (map sort (zip in)))]
+    (->> pairs
+         (map #(abs (apply - %1)))
+         (apply +))))
 
 (defn f2 []
   (let [in (parse-file (slurp *in*))
         [l1 l2] (zip in)]
-    (apply + (map #(similarity-score %1 l2) l1))))
+    (->> l1
+         (map #(similarity-score %1 l2))
+         (apply +))))
 
+;; (println (f1))
 (println (f2))
