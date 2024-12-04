3efe7cbe436376584124053aae6eb46482e7f980
RedPolygon <mail@dantevg.nl>
2024-12-04 18:11:36 +0100

    Dat 3 part 2 (wrong answer)

--- 2024/03.clj
+++ 2024/03.clj
@@ -1,20 +1,36 @@
-(ns adventofcode.2024.03
-  (:require [clojure.string :as str]))
+(ns adventofcode.2024.03)
 
-(defn parse-mul [mul]
-  (->> (apply list mul)
-       (rest)
-   	   (map parse-long)))
+(defn parse-expr [expr]
+  (let [e (rest expr)]
+    (case (first e)
+      "mul" (cons :mul (map parse-long (rest e)))
+      "do" '(:do)
+      "don't" '(:dont))))
 
 (defn parse-file [file]
-  (map parse-mul (re-seq #"mul\((\d+),(\d+)\)" file)))
+  (map parse-expr (re-seq #"(mul|do|don't)\((\d+)?(?:,(\d+))*\)" file)))
+
+(defn mul? [expr] (= :mul (first expr)))
+(defn do? [expr] (= :do (first expr)))
+(defn dont? [expr] (= :dont (first expr)))
+
+(defn calc [exprs]
+  (when (not= exprs '())
+    (concat (->> exprs (take-while (complement dont?)))
+            (->> exprs (drop-while (complement do?)) (rest) (calc)))))
 
 (def in (parse-file (slurp *in*)))
 
 (defn f1 []
-  (apply + (map #(apply * %) in)))
+  (->> in
+       (filter mul?)
+       (map #(apply * (rest %)))
+       (apply +)))
 
 (defn f2 []
-  ())
+  (->> (calc in)
+       (filter mul?)
+       (map #(apply * (rest %)))
+       (apply +)))
 
-(println (f1))
+(println (f2))
